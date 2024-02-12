d1ef48a284e5e140a980f0c7ab5b45092a41a721
Long Nguyen <nguyenv@brandeis.edu>
2024-02-12 11:59:44 -0500

    better buttons

--- script.js
+++ script.js
@@ -34,10 +34,10 @@ function showMessage(response) {
 
     /** Update text content, show message, and change image source to "dance.gif" */
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "LESGOOO see you on the 14th babygirl😘😘";
+    yesMessage.textContent = "YEAAAYYYYYY😘😘";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
-    document.getElementsByClassName("image")[0].src = "images/dance.gif";
+    document.getElementsByClassName("image")[0].src = "images/200w.gif";
 
     /** Remove yes button */
     document.getElementById("yesButton").remove();
